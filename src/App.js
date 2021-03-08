30a393e03026b65d50b46a1aa351399a171c5f37
Ahsan-Ehtesham <ahsanehtesham110@gmail.com>
2021-03-08 05:23:08 +0500

    Counter & Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -1,12 +1,80 @@
-import './App.css';
-import Person from './Person';
+import "./App.css";
+import React, { Component } from "react";
 
-const App=()=> {
-  return (
-    <div className="App">
-      <Person name="Ahsan" genderType="Male"/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Constructor is called");
+    this.state = {
+      count: 0,
+    };
+  }
+
+  // componentWillMount(){
+  //   console.log("Component will mount")
+  // }
+
+  // static getDerivedStateFromProps(props){
+  //   console.log(props)
+  //   console.log("Component will receive props")
+  // }
+  // componentDidMount(){
+  //   console.log("Component did Mount")
+  // }
+
+  // shouldComponentUpdate(){
+  //   console.log("Should Component update")
+  //   return true;
+  // }
+
+  // getSnapshotBeforeUpdate(prevProps,prevState){
+  //   console.log("Prev Props",prevProps)
+  //   console.log("Prev State",prevState)
+  //   console.log("getSnapshotBeforeUpdate is called")
+  // }
+  // componentDidMount(){
+  //   console.log("Component did mount")
+  // }
+
+  // componentWillUnmount(){
+  //   console.log("Component will unmount")
+  // }
+
+  // componentWillMount() {
+  //   this.setState(
+  //     (state) => ({
+  //       count: (state.count = 5),
+  //     }),
+  //     () => {
+  //       console.log(this.state.count);
+  //     }
+  //   );
+  // }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (nextState.count > 25) return false;
+  //   return true;
+  // }
+  // incrementCount = () => {
+  //   this.setState((state) => ({
+  //     count: state.count + 5,
+  //   }));
+  // };
+  // decrementCount = () => {
+  //   this.setState((state) => ({
+  //     count: state.count - 1,
+  //   }));
+  // };
+  render() {
+    return (
+      <div className="counterWrapper">
+        <h1 className="counter">Counter</h1>
+        <p className="count">{this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+        <button onClick={this.decrementCount}>Decrement</button>
+      </div>
+    );
+  }
 }
 
 export default App;
